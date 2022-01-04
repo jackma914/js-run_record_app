e8a5fe3885e7cd2dc4a2c3956c3d76858e5713f7
joon <83916101+jackma914@users.noreply.github.com>
2022-01-04 20:11:11 +0900

    add
    add

--- script1.js
+++ script1.js
@@ -14,6 +14,49 @@ const months = [
   'November',
   'December',
 ];
+
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+
+  constructor(coords, distance, duration) {
+    this.coords = coords;
+    this.distance = distance; // in km
+    this.duration = duration; // in min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    //min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    //km/h
+    this.speed = this.distance / this.duration;
+    return this.speed;
+  }
+}
+
+// const run1 = new Running([39, -12], 4.2, 24, 167);
+// console.log(run1);
+// application architecture
+
 // APPLICATION ARCHITECTURE
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
@@ -79,6 +122,17 @@ class App {
   newWorkout(e) {
     e.preventDefault();
 
+    const type = inputType.value;
+    const distance = +inputDistance.value;
+    const duration = +inputDuration.value;
+
+    if (type === 'running') {
+      const cadence = +inputCadence.value;
+    }
+
+    if (type === 'cycling') {
+      const elevation = +inputElevation.value;
+    }
     //clear input fields
     inputDistance.value =
       inputDuration.value =
