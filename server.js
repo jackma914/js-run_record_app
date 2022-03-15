66f29dd09b58c0e0290dccd2156b4078c8ef013f
joon <83916101+jackma914@users.noreply.github.com>
2022-03-15 19:35:37 +0900

    ADD
    ADD

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.use(express.static(path.resolve(__dirname, './build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './build,"index.html'));
+});
+build;
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
