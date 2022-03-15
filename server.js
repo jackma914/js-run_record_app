6cdbe8dfb8dd0ad8d34a416c867516302b70f083
joon <83916101+jackma914@users.noreply.github.com>
2022-03-15 20:19:17 +0900

    ADD
    ADD

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const path = require('path');
 app.use(express.static(path.resolve(__dirname, './')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './,"index.html'));
+  res.sendFile(path.resolve(__dirname, './index.html'));
 });
 
 app.listen(port, () => {
