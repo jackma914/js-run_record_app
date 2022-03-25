fa2f9257f53ed0f7a9bb00bb05a41c0593b92c1f
Junhyeok Sim <83916101+jackma914@users.noreply.github.com>
2022-03-25 17:40:23 +0900

    Initialise project
    프로젝트 초기화 커밋입니다.

---
+++ sctipt.js
@@ -0,0 +1,319 @@
+'use strict';
+
+const clickMap = document.getElementById('map');
+const section = document.querySelector('.section');
+const navTogglerBtn = document.querySelector('.nav-toggler');
+const sidebar = document.querySelector('.sidebar');
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Workout {
+  date = new Date();
+
+  id = (Date.now() + '').slice(-10);
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; // km
+    this.duration = duration; // min
+  }
+
+  setDescription() {
+    // prettier-ignore
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${
+      months[this.date.getMonth()]
+    } ${this.date.getDate()}`;
+  }
+}
+
+class Running extends Workout {
+  type = 'running';
+
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+    this.setDescription();
+  }
+
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+class Cycling extends Workout {
+  type = 'cycling';
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+    this.setDescription();
+  }
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class App {
+  #map;
+  #mapEvent;
+  #workouts = [];
+  #mapZoomLevel = 13;
+
+  constructor() {
+    // 사용자 위치를 받습니다.
+    this.getPosition();
+
+    // local storage에서 데이터를 가져옵니다.
+    this.getLocalStorage();
+
+    // event handlers 연결
+    form.addEventListener('submit', this.newWorkout.bind(this));
+    inputType.addEventListener('change', this.toggleElevationField);
+    containerWorkouts.addEventListener('click', this.moveToPopup.bind(this));
+    clickMap.addEventListener('click', this.asideSectionTogglerBtn.bind(this));
+  }
+
+  asideSectionTogglerBtn() {
+    sidebar.classList.toggle('open');
+    navTogglerBtn.classList.toggle('open');
+    // section.classList.toggle('open');
+  }
+
+  getPosition() {
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(
+        this.loadMap.bind(this),
+        function () {
+          return alert('위치정보를 찾을수 없습니다.');
+        }
+      );
+  }
+
+  loadMap(position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    const coords = [latitude, longitude];
+    this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    // 지도 클링 handling
+    // 자바스크립트의 메서드가아닌 leaflet의 on 메서드 입니다.
+    this.#map.on('click', this.showForm.bind(this));
+
+    this.#workouts.forEach(work => {
+      this.renderWorkoutMarker(work);
+    });
+  }
+
+  showForm(mapE) {
+    this.#mapEvent = mapE;
+
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+
+  hideForm() {
+    // input 초기화
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
+    form.style.display = 'none';
+    form.classList.add('hidden');
+    setTimeout(() => (form.style.display = 'grid'), 1000);
+  }
+
+  toggleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  newWorkout(e) {
+    // 유효성 검사 도구 validator
+    const validInputs = (...inputs) =>
+      inputs.every(inp => Number.isFinite(inp));
+    const allPositive = (...inputs) => inputs.every(inp => inp > 0);
+
+    e.preventDefault();
+
+    // form 에서 데이터 받아옵니다.
+    const type = inputType.value;
+    const distance = +inputDistance.value;
+    const duration = +inputDuration.value;
+    const { lat, lng } = this.#mapEvent.latlng;
+    let workout;
+
+    // workout running 이면, running 객체 생성
+    if (type === 'running') {
+      const cadence = +inputCadence.value;
+      // 데이터 유효성 검사
+      if (
+        !validInputs(duration, distance, cadence) ||
+        !allPositive(distance, duration, cadence)
+        // !Number.isFinite(distance) ||
+        // !Number.isFinite(duration) ||
+        // !Number.isFinite(cadence)
+      )
+        return alert('숫자와 양수만 입력 가능합니다. ');
+
+      workout = new Running([lat, lng], distance, duration, cadence);
+    }
+
+    // workout cycling 이면, cycling 객체 생성
+    if (type === 'cycling') {
+      if (type === 'cycling') {
+        const elevation = +inputElevation.value;
+        // 데이터 유효성 검사
+        if (
+          !validInputs(duration, distance, elevation) ||
+          !allPositive(distance, duration)
+          // !Number.isFinite(distance) ||
+          // !Number.isFinite(duration) ||
+          // !Number.isFinite(cadence)
+        )
+          return alert('숫자와 양수만 입력 가능합니다. (Elev Gain 음수 가능) ');
+
+        workout = new Cycling([lat, lng], distance, duration, elevation);
+      }
+    }
+
+    // workout 배열에 새 객체 추가
+    this.#workouts.push(workout);
+
+    // 마커로 지도에 workout 렌더링
+    this.renderWorkoutMarker(workout);
+
+    // 리스트에 workout 렌더링
+    this.renderWorkout(workout);
+
+    //hide form + input 필드 초기화
+    this.hideForm();
+
+    // 모든 workouts을 local storage 설정
+    this.setLocalStorage();
+  }
+
+  renderWorkoutMarker(workout) {
+    L.marker(workout.coords)
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: `${workout.type}-popup`,
+        })
+      )
+      .setPopupContent(
+        `${workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'} ${workout.description}`
+      )
+      .openPopup();
+  }
+
+  renderWorkout(workout) {
+    let html = `
+          <li class="workout workout--${workout.type}" data-id="${workout.id}">
+          <h2 class="workout__title">${workout.description}</h2>
+          <div class="workout__details">
+            <span class="workout__icon">${
+              workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'
+            }</span>
+            <span class="workout__value">${workout.distance}</span>
+            <span class="workout__unit">km</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">⏱</span>
+            <span class="workout__value">${workout.duration}</span>
+            <span class="workout__unit">min</span>
+          </div>`;
+
+    if (workout.type === 'running')
+      html += `
+    <div class="workout__details">
+            <span class="workout__icon">⚡️</span>
+            <span class="workout__value">${workout.pace.toFixed(1)}</span>
+            <span class="workout__unit">min/km</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">🦶🏼</span>
+            <span class="workout__value">${workout.cadence}</span>
+            <span class="workout__unit">spm</span>
+          </div>
+          </li>
+          `;
+
+    if (workout.type === 'cycling')
+      html += `  <div class="workout__details">
+            <span class="workout__icon">⚡️</span>
+            <span class="workout__value">${workout.speed.toFixed(1)}</span>
+            <span class="workout__unit">km/h</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">⛰</span>
+            <span class="workout__value">${workout.elevationGain}</span>
+            <span class="workout__unit">m</span>
+          </div>
+        </li>`;
+
+    form.insertAdjacentHTML('afterend', html);
+  }
+
+  moveToPopup(e) {
+    const workoutEl = e.target.closest('.workout');
+    console.log(workoutEl);
+
+    if (!workoutEl) return;
+
+    const workout = this.#workouts.find(
+      work => work.id === workoutEl.dataset.id
+    );
+    console.log(workout);
+
+    this.#map.setView(workout.coords, this.#mapZoomLevel, {
+      animate: true,
+      pan: {
+        duration: 1,
+      },
+    });
+  }
+
+  setLocalStorage() {
+    //localstorage에 데이터 저장
+    localStorage.setItem('workouts', JSON.stringify(this.#workouts));
+  }
+
+  getLocalStorage() {
+    //localstorage에 데이터 가져옵니다.
+    const data = JSON.parse(localStorage.getItem('workouts'));
+    console.log(data);
+
+    if (!data) return;
+
+    this.#workouts = data;
+    console.log(this.#workouts);
+
+    this.#workouts.forEach(work => {
+      this.renderWorkout(work);
+    });
+  }
+}
+
+const app = new App();
