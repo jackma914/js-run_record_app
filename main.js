bd23e7f065c84b77f690de887737cef0ecfebb38
Junhyeok Sim <83916101+jackma914@users.noreply.github.com>
2022-03-25 18:35:27 +0900

    ADD get current position
    geolocation을 이용해서 현재 위치를 가져 왔습니다.

--- main.js
+++ main.js
@@ -10,3 +10,16 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// 콜백과 오류 콜백 인자 두개를 받습니다.
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+    },
+    function () {
+      alert('위치 정보를 받아오지 못했습니다.');
+    }
+  );
+}
