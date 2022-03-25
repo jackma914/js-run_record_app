50c8f503b89c86b6deea0b4382b46eacdfbf34f3
Junhyeok Sim <83916101+jackma914@users.noreply.github.com>
2022-03-26 02:14:16 +0900

    ADD workout input form render
    workout input form의 submit되면 마커가 표시 되도록 구현, input form의 type select의 토글을 구현하였습니다.

--- main.js
+++ main.js
@@ -11,6 +11,9 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+// 글로벌 변수(전역 변수)
+let map, mapEvent;
+
 // 콜백과 오류 콜백 인자 두개를 받습니다.
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
@@ -19,7 +22,9 @@ if (navigator.geolocation) {
       const { longitude } = position.coords;
 
       const coords = [latitude, longitude];
-      const map = L.map('map').setView(coords, 13);
+
+      // map 함수는 글로벌 변수로 만들었습니다.
+      map = L.map('map').setView(coords, 13);
 
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
@@ -27,23 +32,11 @@ if (navigator.geolocation) {
       }).addTo(map);
 
       // 클릭한 곳의 위치정보를 받기 위해 map.on 메서드를 사용합니다.
-      map.on('click', function (mapEvent) {
-        const { lat, lng } = mapEvent.latlng;
-
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            //팝업 마커를 커스텀을 위해 L.popup 메서드를 이용해 옵션을 넣어줍니다.
-            L.popup({
-              maxWidth: 250,
-              minWidth: 100,
-              autoClose: false,
-              closeOnClick: false,
-              className: 'running-popup',
-            })
-          )
-          .setPopupContent('workout')
-          .openPopup();
+      map.on('click', function (mapE) {
+        //받아온 mapE는 클릭한 위치정보입니다. 글로벌 변수 mapEvent에 데이터를 복사하였습니다..
+        mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
       });
     },
     function () {
@@ -51,3 +44,37 @@ if (navigator.geolocation) {
     }
   );
 }
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  console.log(inputDuration);
+
+  // 이벤트가 발생하고 input를 초기화 해주었습니다.
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
+
+  // 마커 표시
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      //팝업 마커를 커스텀을 위해 L.popup 메서드를 이용해 옵션을 넣어줍니다.
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('workout')
+    .openPopup();
+});
+
+inputType.addEventListener('change', function () {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
--- text.md
+++ text.md
@@ -20,3 +20,71 @@
         console.log(position);
       });
       ```
+    - popup을 커스텀 하기 위해 L.popup()메서드를 사용해 옵션을 넣어줍니다.
+
+3.  사용자가 지도를 클릭하면 workout input form이 렌더링 됩니다.
+
+    - map.on()메서드로 클릭시 form에 "hidden"를 만들어서 클릭시 "hidden" 클레스가 remove되도록 구현했습니다.
+
+      ```js
+      map.on('click', function (mapE) {
+        //받아온 mapE는 클릭한 위치정보입니다. 글로벌 변수 mapEvent에 데이터를 복사하였습니다..
+        mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+      });
+      ```
+
+    - !imoportant 생성됨 form에서 데이터를 입력후 데이터를 submit하는 form.addEventListener() 메서드를 만듭니다. geolocation과는 무관하기 때문에 분리해서 구현했습니다. form 에서 넘어온 데이터를 marker와 연결 해야하기때문에 L.marker 메서드를 form.addEventListener()로 들여옵니다. 여기서 발생하는 두가지 문제가 있습니다. mapEvent와 map 함수의 데이터를 받을수가 없게 되었습니다. 이를 해결하기 위해 geolocation 속에 들어있던 두가지의 데이터를 글로벌 변수(전역 변수)로 만들어 주었습니다.
+
+      ```js
+      // 글로벌 변수(전역 변수)
+      let map, mapEvent;
+
+      // 콜백과 오류 콜백 인자 두개를 받습니다.
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          function (position) {
+            const { latitude } = position.coords;
+            const { longitude } = position.coords;
+
+            const coords = [latitude, longitude];
+
+            // map 함수는 글로벌 변수로 만들었습니다.
+            map = L.map('map').setView(coords, 13);
+
+            L.tileLayer(
+              'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
+              {
+                attribution:
+                  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+              }
+            ).addTo(map);
+
+            // 클릭한 곳의 위치정보를 받기 위해 map.on 메서드를 사용합니다.
+            map.on('click', function (mapE) {
+              //받아온 mapE는 클릭한 위치정보입니다. 글로벌 변수 mapEvent에 데이터를 복사하였습니다..
+              mapEvent = mapE;
+              form.classList.remove('hidden');
+              inputDistance.focus();
+            });
+          },
+          function () {
+            alert('위치 정보를 받아오지 못했습니다.');
+          }
+        );
+      }
+      ```
+
+    - workout input form의 type을 closest 메서드와 toggle 메서드를 이용해 토글을 구현하겠습니다.
+      중요한점은 토글을 구현하기 위해 html을 부모요소와 hidden 클레스가 필요합니다.
+      ```js
+      inputType.addEventListener('change', function () {
+        inputElevation
+          .closest('.form__row')
+          .classList.toggle('form__row--hidden');
+        inputCadence
+          .closest('.form__row')
+          .classList.toggle('form__row--hidden');
+      });
+      ```
